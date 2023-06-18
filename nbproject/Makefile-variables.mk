f49fb5c14c97025a842358853564ed00cd2ad1d6
Ishwae Sonawane <Ishwae Sonawane@ISHWAR-PC>
2023-06-18 15:51:45 +0530

    First trial git version control

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=GitTrial.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/GitTrial.X.production.hex
